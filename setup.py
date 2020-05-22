9c1d542f6823defafb252e570a02a91a90d54ba1
lewisgmorris <lewis.morris@gmail.com>
2020-05-22 10:46:54 +0100

    image_bar updated - slow but working

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='progplot',
-    version="0.1.5",
+    version="0.1.6",
     packages=['progplot'],
     url='https://github.com/lewis-morris/progplot',
     license='MIT',
